05c6502b462512b531a23ed81cbfcd6c56f31268
amena-PC\amena <amenaikhn@yahoo.com>
2016-11-09 10:02:16 -0500

    Henry's changes - Questions and Answers

--- Munchies/Assets/GameControl.cs
+++ Munchies/Assets/GameControl.cs
@@ -2,16 +2,19 @@
 using System.Collections;
 
 public class GameControl : MonoBehaviour {
+	
 
 	public Camera cam;
 	public GameObject food; 
+	public GameObject Heading, newObject;
 	//public GameObject food2; 
 	private float maxWidth;
-
+	private UnityEngine.UI.Text question, foodText;
+	private string[,] solutions = new string[5,2] {{"What is a word to describe somebody who is talented beyond their age?","precocious"},{"What is a word to describe a leader gaining support by speaking to peoples interests?","demagogue"},{"What is a word to describe something out-of-date?", "anachronistic"},{"What is a word for the opposite of loyal?","perfidious"},{"What is another word for impulsive?","impetuous"}};
 
 	// Use this for initialization
 	void Start () {
-
+		Debug.Log("Hello");
 		if (cam == null)
 		{
 			cam = Camera.main;
@@ -24,14 +27,18 @@ public class GameControl : MonoBehaviour {
 
 	IEnumerator Spawn (){
 		//Need x,y,z
-		while (true) {
+		for (int i = 0; i < 5; i++) {
+			question = Heading.GetComponent<UnityEngine.UI.Text>();
+			question.text = solutions [i, 0];
 			yield return new WaitForSeconds (2.0f); 
 			Vector3 spawnPosition = new Vector3 (
 				                        Random.Range (-maxWidth, maxWidth),
 				                        transform.position.y,
 				                        -1.0f);
-			Quaternion spawnRotation = Quaternion.identity; 
-			Instantiate (food, spawnPosition, spawnRotation);
+			Quaternion spawnRotation = Quaternion.identity;
+			GameObject newObject = (GameObject)Instantiate (food, spawnPosition, spawnRotation);
+			TextMesh hello = newObject.transform.GetComponent<TextMesh> ();
+			hello.text = solutions [i, 1];
 			//Instantiate (food2, spawnPosition, spawnRotation); 
 			yield return new WaitForSeconds (Random.Range (1.0f, 2.0f));
 		}
