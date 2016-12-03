b23090beafd64b903bdc7723fe976d14f89c80ad
Charles <cmarullo308@gmail.com>
2016-12-03 16:19:06 -0500

    Score Update

--- Munchies/Assets/scripts/destroyonContact.cs
+++ Munchies/Assets/scripts/destroyonContact.cs
@@ -2,8 +2,12 @@
 using System.Collections;
 
 public class destroyonContact : MonoBehaviour {
+	int score = 0;
+	Text scoreText = GetComponent (UI.ScoreText);
+
 	void OnTriggerEnter2D (Collider2D other)
 	{
+		score++;
 		Destroy (other.gameObject);
 	}
 }
