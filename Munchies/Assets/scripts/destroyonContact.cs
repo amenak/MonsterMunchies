ee99a84707821d3f2c45f5014622f60eb3ef0f66
Charles <cmarullo308@gmail.com>
2016-12-03 20:51:08 -0500

    Fix

--- Munchies/Assets/scripts/destroyonContact.cs
+++ Munchies/Assets/scripts/destroyonContact.cs
@@ -1,13 +1,26 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class destroyonContact : MonoBehaviour {
-	int score = 0;
-	Text scoreText = GetComponent (UI.ScoreText);
+	int score;
+	public GUIText ScoreText;
+
+	void Start()
+	{
+		score = 0;
+		UpdateScore ();
+	}
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		score++;
+		Debug.Log (score);
 		Destroy (other.gameObject);
+	}
+
+	void UpdateScore()
+	{
+		ScoreText.text = "Score: " + score;
 	}
 }
